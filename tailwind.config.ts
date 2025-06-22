615b12f2e8673224d646142750a43585c0919e0e
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-22 19:41:41 +0530

    Updated code files: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				sans: ["Inter", "system-ui", "sans-serif"],
+				sans: ["Inter", ...fontFamily.sans],
 			},
 			colors: {
 				border: 'hsl(var(--border))',
